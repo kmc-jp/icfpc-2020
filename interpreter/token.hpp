a97d8f8c9543eb3a4481c69f31758fcf01e411e8
primenumber <prime@kmc.gr.jp>
2020-07-18 05:31:40 +0900

    Update token.hpp

--- interpreter/token.hpp
+++ interpreter/token.hpp
@@ -1,32 +1,33 @@
 #include <string>
 
 enum class TokenType {
-  Number,
+  Number, // 0
   Equality,
   Succ,
   Pred,
   Variable,
-  Sum,
+  Sum, // 5
   Product,
   Division,
   Eq,
   Lt,
-  Modulate,
+  Modulate, // 10
   Demod,
   Send,
   Negate,
   Apply,
-  S,
+  S, // 15
   C,
   B,
   True,
   False,
-  Pwr2,
+  Pwr2, // 20
   I,
   Cons,
   Car,
   Cdr,
-  Nil,
+  Nil, // 25
+  IsNil,
   Partial
 };
 
@@ -36,9 +37,15 @@ struct Token {
 };
 
 constexpr Token app = {TokenType::Apply, 0};
+constexpr Token inc = {TokenType::Succ, 0};
+constexpr Token dec = {TokenType::Pred, 0};
 constexpr Token sum = {TokenType::Sum, 0};
+constexpr Token mul = {TokenType::Product, 0};
+constexpr Token eq = {TokenType::Eq, 0};
+constexpr Token comb_s = {TokenType::S, 0};
 constexpr Token comb_c = {TokenType::C, 0};
-constexpr Token comb_b = {TokenType::C, 0};
+constexpr Token comb_b = {TokenType::B, 0};
+constexpr Token pwr2 = {TokenType::Pwr2, 0};
 constexpr Token cons = {TokenType::Cons, 0};
 constexpr Token nil = {TokenType::Nil, 0};
 constexpr Token number(int64_t x) { return {TokenType::Number, x}; }
