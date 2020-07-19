1a99cc3b9f1ebf2296cbe0fdb3bc90569b38caae
yamunaku <chuta@snowkids.jp>
2020-07-20 05:55:58 +0900

    Update player.cpp

--- app/player.cpp
+++ app/player.cpp
@@ -1,5 +1,11 @@
 #include "player.hpp"
 
+int sign(int x){
+  if (x > 0) return 1;
+  else if (x < 0) return -1;
+  else return 0;
+}
+
 std::vector<CommandPtr> Player(GameResponse gameResponse) {
 
   if (gameResponse.status == 0) return {};
@@ -11,7 +17,7 @@ std::vector<CommandPtr> Player(GameResponse gameResponse) {
   for(auto &sc : gameResponse.gameState.shipsAndCommands){
     Ship &ship = sc.ship;
     if (ship.role == myRole) {
-      com.push_back(make_accelerate(ship.shipId, {ship.position.x, ship.position.y}));
+      com.push_back(make_accelerate(ship.shipId, {sign(ship.position.x), sign(ship.position.y)}));
     }
   }
 
