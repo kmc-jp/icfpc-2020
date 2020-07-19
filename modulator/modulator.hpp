1fbc2a4404b39e7644f4bd415ebc5bcc07d5a8b1
yamunaku <chuta@snowkids.jp>
2020-07-19 20:52:13 +0900

    delete modulator directory

--- modulator/modulator.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "lexer.hpp"
-#include "interpreter.hpp"
-
-string string_of_int(int x);
-
-void parse_list(const string &s, int &idx);
-
-string modulate(const string s);
