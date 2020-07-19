653c2aef8dcfb9c6fd8f0d7c9e7f4e4bef76cf9b
Yamazaki Hiroki <yamazaki.hiroki.58r@gmail.com>
2020-07-19 21:41:32 +0900

    add response.hpp

--- app/main.cpp
+++ app/main.cpp
@@ -5,16 +5,9 @@
 #include "../interpreter/token.hpp"
 #include "../interact/demodulator.hpp"
 #include "../interact/modulator.hpp"
+#include "response.hpp"
 
 const int GAME_FINISHED = 2;
-
-struct GameResponse{
-	int status; // wrong なら 0
-	int gameStage; // 0-> not started, 1-> started, 2->finished
-	std::vector<int> staticGameInfo; // (x0, role, x2, x3, x4)
-	// role 0-> attacker, 1->defender
-	//std::vector<int> gameState; // (gameTick, x1, shipsAndCommands) 
-};
 
 // 10進数playerKey -> [2, playerKey, nil]
 std::string makeJoinRequest(std::string playerKey){
